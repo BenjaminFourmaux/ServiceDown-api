1c8a7191255dd2c16fc7e88100a9806db89d7d3d
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2022-05-26 18:23:57 +0200

    Add get only one country

--- api/views/country.py
+++ api/views/country.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -16,3 +17,12 @@ class CountryViewSet(viewsets.ModelViewSet):
         serializer = self.serializer_class(countries, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance: Country = self.get_object()
+
+        if instance and instance.isAvailable:
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
