023f1203536207016773a72d9fd1e481a8cb999f
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2022-10-13 11:36:38 +0200

    Custom check_db command

--- api/management/commands/check_db.py
+++ api/management/commands/check_db.py
@@ -1,30 +1,33 @@
 # check_db.py
-
 import socket
 import time
-import argparse
-""" Check if port is open, avoid docker-compose race condition """
+from django.core.management import BaseCommand, call_command
 
-parser = argparse.ArgumentParser(description='Check if port is open, avoid\
-                                 docker-compose race condition')
-parser.add_argument('--service-name', required=True)
-parser.add_argument('--ip', required=True)
-parser.add_argument('--port', required=True)
 
-args = parser.parse_args()
+class Command(BaseCommand):
+    help = "Check if port is open, avoid docker-compose race condition"
 
-# Get arguments
-service_name = str(args.service_name)
-port = int(args.port)
-ip = str(args.ip)
+    def add_arguments(self, parser):
+        parser.add_argument('service_name', help="Name of BD service")
+        parser.add_argument('ip', help="IP address of DB or Docker name")
+        parser.add_argument('port', help="Port numbers of BD")
 
-# Infinite loop
-while True:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((ip, port))
-    if result == 0:
-        print("{0} port is open! Bye!".format(service_name))
-        break
-    else:
-        print("{0} port is not open! I'll check it soon!".format(service_name))
-        time.sleep(3)
+    def handle(self, *args, **options):
+        """ Check if port is open, avoid docker-compose race condition """
+
+        self.stdout.write('Check Database connection')
+
+        service_name = options['service_name']
+        ip = options['ip']
+        port = options['port']
+
+        # Infinite loop
+        while True:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                print("{0} port is open! Bye!".format(service_name))
+                break
+            else:
+                print("{0} port is not open! I'll check it soon!".format(service_name))
+                time.sleep(3)
